6e6e5939080f93c8efa0211f1d6be68c9b9e90a7
Bharath <bharath98mohan@gmail.com>
2020-06-27 15:23:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const User = ({match}) => {
+return(<h1>Welcome User {match.params.username}</h1>)
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <ul>
+          <li><NavLink to="/" exact activeStyle={{color:'green'}}>Home</NavLink></li>
+          <li><NavLink to="/about" exact activeStyle={{color:'green'}}>About</NavLink></li>
+          <li><NavLink to="/user/John" exact activeStyle={{color:'green'}}>User John</NavLink></li>
+          <li><NavLink to="/user/Peter" exact activeStyle={{color:'green'}}>User Peter</NavLink></li>
+        </ul>
+        <div className="App">
+        <Route path="/" exact render= {
+          () => {
+            return( <h1>Welcome home</h1>);
+          }
+        }/>
+
+          <Route path="/about" exact strict render= {
+          () => {
+            return( <h1>Welcome about</h1>);
+          }
+        }/>
+
+          <Route path="/user/:username" exact strict component={User}/>
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
